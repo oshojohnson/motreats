bf4589f388e39d175981ee35aa347341f0a18763
Johnson Osho <oshojohnson@gmail.com>
2021-10-29 15:59:03 +0100

    Updates

---
+++ precache-manifest.8fb62290e13e71e6016b87930cedad5d.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eed1ee4543afcf1778ea3572c963fe1",
+    "url": "/motreats/index.html"
+  },
+  {
+    "revision": "3eaf74728b61eeb1e305",
+    "url": "/motreats/static/css/2.5556ba3d.chunk.css"
+  },
+  {
+    "revision": "a1fbc949d9cabccc12f8",
+    "url": "/motreats/static/css/main.d3b155e0.chunk.css"
+  },
+  {
+    "revision": "3eaf74728b61eeb1e305",
+    "url": "/motreats/static/js/2.807e3f55.chunk.js"
+  },
+  {
+    "revision": "52515fbe39da5d1c18602faa1b7dfd69",
+    "url": "/motreats/static/js/2.807e3f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1fbc949d9cabccc12f8",
+    "url": "/motreats/static/js/main.d970043e.chunk.js"
+  },
+  {
+    "revision": "9ecfb141af661d0190e7",
+    "url": "/motreats/static/js/runtime-main.fd0fa1db.js"
+  },
+  {
+    "revision": "1be206d892519d1ecb0e5941015754a1",
+    "url": "/motreats/static/media/Flaticon.1be206d8.svg"
+  },
+  {
+    "revision": "20815c7463ea46e5ecd6b23f06a6ce1c",
+    "url": "/motreats/static/media/Flaticon.20815c74.woff2"
+  },
+  {
+    "revision": "62694bffc0222ca238bacb1a52b63f05",
+    "url": "/motreats/static/media/Flaticon.62694bff.woff"
+  },
+  {
+    "revision": "d84c6aaa62a933491f8db070616786de",
+    "url": "/motreats/static/media/Flaticon.d84c6aaa.ttf"
+  },
+  {
+    "revision": "e5d3dab7a3c809a8cd9962d0165d44ba",
+    "url": "/motreats/static/media/Flaticon.e5d3dab7.eot"
+  },
+  {
+    "revision": "2621b4de75e9f93e53f71224cea23c93",
+    "url": "/motreats/static/media/bg1.2621b4de.jpg"
+  },
+  {
+    "revision": "18816c4f5c2f38a71ccea01bab0ed371",
+    "url": "/motreats/static/media/bg2.18816c4f.jpg"
+  },
+  {
+    "revision": "57553a6addb7da66bd88a2335c7a74ed",
+    "url": "/motreats/static/media/bg3.57553a6a.jpg"
+  },
+  {
+    "revision": "0ce4d4c041a8d83dc0106b697a92ac65",
+    "url": "/motreats/static/media/bg4.0ce4d4c0.jpg"
+  },
+  {
+    "revision": "74609cce2a2d5133322f11e49cd7a917",
+    "url": "/motreats/static/media/bg5.74609cce.jpg"
+  },
+  {
+    "revision": "6c7b3ec680f7ebf8d504d9a2427f88ff",
+    "url": "/motreats/static/media/bnr1.6c7b3ec6.jpg"
+  },
+  {
+    "revision": "a135215dc1353355ab63753ff425f5a4",
+    "url": "/motreats/static/media/cake1.a135215d.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/motreats/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/motreats/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/motreats/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/motreats/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/motreats/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "131b7f1e91a652791f08f5ccfe702645",
+    "url": "/motreats/static/media/line-awesome.131b7f1e.svg"
+  },
+  {
+    "revision": "3f85d8035b4ccd91d2a1808dd22b7684",
+    "url": "/motreats/static/media/line-awesome.3f85d803.eot"
+  },
+  {
+    "revision": "452a5b42cb4819f09d35bcf6cbdb24c1",
+    "url": "/motreats/static/media/line-awesome.452a5b42.woff2"
+  },
+  {
+    "revision": "4d42f5f0c62a8f51e876c14575354a6e",
+    "url": "/motreats/static/media/line-awesome.4d42f5f0.ttf"
+  },
+  {
+    "revision": "8b1290595e57e1d49d95ff3fa1129ecc",
+    "url": "/motreats/static/media/line-awesome.8b129059.woff"
+  },
+  {
+    "revision": "8ad6fa9c298a7c2d3bc49b8db42d71e6",
+    "url": "/motreats/static/media/logo.8ad6fa9c.png"
+  },
+  {
+    "revision": "4fb43442d2d5e39743e3e6ad8151fccd",
+    "url": "/motreats/static/media/logo1.4fb43442.jpg"
+  },
+  {
+    "revision": "bfe11ed44a1fb52876da55d7745bf7ea",
+    "url": "/motreats/static/media/logo2.bfe11ed4.jpg"
+  },
+  {
+    "revision": "8c6e026bc12edab65b06e31ff379c416",
+    "url": "/motreats/static/media/logo3.8c6e026b.jpg"
+  },
+  {
+    "revision": "5c809f3f80d6dfb4518844c55a3be225",
+    "url": "/motreats/static/media/logo4.5c809f3f.jpg"
+  },
+  {
+    "revision": "3de091e4413b15d43e53dc56a8f9e00d",
+    "url": "/motreats/static/media/logo5.3de091e4.jpg"
+  },
+  {
+    "revision": "afb9baafaaf83a81c727c1cac76cc258",
+    "url": "/motreats/static/media/logo6.afb9baaf.jpg"
+  },
+  {
+    "revision": "1c1e8ec04aae7732164b15bb37f1b502",
+    "url": "/motreats/static/media/map.1c1e8ec0.png"
+  },
+  {
+    "revision": "ea01aebda7b3001a0f1505bde2db05c7",
+    "url": "/motreats/static/media/member1.ea01aebd.jpg"
+  },
+  {
+    "revision": "d02953c20dd7f5cd5789ca796abdcdf5",
+    "url": "/motreats/static/media/member2.d02953c2.jpg"
+  },
+  {
+    "revision": "ed5d998f369b4708cb70b597200722b7",
+    "url": "/motreats/static/media/member3.ed5d998f.jpg"
+  },
+  {
+    "revision": "ecf0d6e0923ad7f8b1663e35a8927f34",
+    "url": "/motreats/static/media/member4.ecf0d6e0.jpg"
+  },
+  {
+    "revision": "8ad6fa9c298a7c2d3bc49b8db42d71e6",
+    "url": "/motreats/static/media/newlogo.8ad6fa9c.png"
+  },
+  {
+    "revision": "9cc3f466fd5f28ec356ea14c819ec8a1",
+    "url": "/motreats/static/media/pattern.9cc3f466.svg"
+  },
+  {
+    "revision": "45eaf82073b505e9b6805ea10672bb59",
+    "url": "/motreats/static/media/pic1.45eaf820.jpg"
+  },
+  {
+    "revision": "b66be14578d8c4780294b3be96aece0b",
+    "url": "/motreats/static/media/pic3.b66be145.jpg"
+  },
+  {
+    "revision": "a8bc511e31e440b9f9f38d071c9c4fa3",
+    "url": "/motreats/static/media/pic4.a8bc511e.jpg"
+  },
+  {
+    "revision": "a1e1773b35a34ab1c99f4438e72aa3da",
+    "url": "/motreats/static/media/pic5.a1e1773b.jpg"
+  },
+  {
+    "revision": "087f3214ccb6150f8391008fd14155fe",
+    "url": "/motreats/static/media/pic6.087f3214.jpg"
+  },
+  {
+    "revision": "fe663f10fda7830dc2890a85ebb00862",
+    "url": "/motreats/static/media/pic7.fe663f10.jpg"
+  },
+  {
+    "revision": "9e7b0e2de136bf7f3f8f56828aaa0bbf",
+    "url": "/motreats/static/media/slide1.9e7b0e2d.jpg"
+  },
+  {
+    "revision": "367450de3b2291ac896432c1bf156cac",
+    "url": "/motreats/static/media/slide2.367450de.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/motreats/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/motreats/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/motreats/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/motreats/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "879a3fdf60d520cf1b3e71dbd21ee9ce",
+    "url": "/motreats/static/media/thum1.879a3fdf.jpg"
+  }
+]);
